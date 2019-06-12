0262b5b52546b35c78f8d57d4c09d50c3e4523fc
aoi <s19006@std.it-college.ac.jp>
2019-06-12 12:49:22 +0900

    6/12課題

---
+++ 0612/060403.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+usage()
+{
+    local script_name=$(basename "$0")
+
+  cat << END
+使い方:060403.sh DIRPATH...
+DIRPATHで指定したディレクトリ下の実行可能ファイルの一覧表示
+FILEPATH - 実行可能ファイルの一覧表示を行うディレクトリパスを指定
+END
+}
+
+if [ "$#" -eq 0 ]; then
+    usage
+    exit 1
+fi
+if [ -d "$1" ]; then
+    #for file in $(ls "$1")
+    for file in $(find $1 -maxdepth 1 -type f)
+    do
+        if [ -x "$file" ]; then
+            echo $(basename "$file")
+        fi
+    done
+else
+    echo "$1":'ディレクトリではありません'
+ fi
